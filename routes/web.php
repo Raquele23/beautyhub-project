27ef54b9288e628f5d13848a080b439157a79359
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2026-03-07 19:22:45 -0300

    feat: adiciona rotas para criação, edição e listagem de serviços e portfólio profissional

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProfessionalController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,30 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Rotas para Profissionais
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Perfil Profissional
+    Route::get('/professional/create', [ProfessionalController::class, 'create'])->name('professional.create');
+    Route::post('/professional', [ProfessionalController::class, 'store'])->name('professional.store');
+    Route::get('/professional', [ProfessionalController::class, 'show'])->name('professional.show');
+    Route::get('/professional/edit', [ProfessionalController::class, 'edit'])->name('professional.edit');
+    Route::patch('/professional', [ProfessionalController::class, 'update'])->name('professional.update');
+
+    // Portfólio
+    Route::post('/professional/portfolio', [ProfessionalController::class, 'addPortfolioPhoto'])->name('professional.portfolio.add');
+    Route::delete('/professional/portfolio/{photo}', [ProfessionalController::class, 'deletePortfolioPhoto'])->name('professional.portfolio.delete');
+
+    // Serviços
+    Route::prefix('services')->group(function () {
+        Route::get('/', [ServiceController::class, 'index'])->name('services.index');
+        Route::get('/create', [ServiceController::class, 'create'])->name('services.create');
+        Route::post('/', [ServiceController::class, 'store'])->name('services.store');
+        Route::get('/{service}/edit', [ServiceController::class, 'edit'])->name('services.edit');
+        Route::patch('/{service}', [ServiceController::class, 'update'])->name('services.update');
+        Route::delete('/{service}', [ServiceController::class, 'destroy'])->name('services.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
