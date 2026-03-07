e94843b2679014a24063564802aa2aaa02ba0b44
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2026-03-07 19:18:18 -0300

    feat: adiciona model PortfolioPhoto com relacionamento com Professional

---
+++ app/Models/PortfolioPhoto.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Professional;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PortfolioPhoto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'portfolio_photos';
+
+    protected $fillable = [
+        'professional_id',
+        'photo',
+        'description',
+    ];
+
+    public function professional(): BelongsTo
+    {
+        return $this->belongsTo(Professional::class);
+    }
+}
