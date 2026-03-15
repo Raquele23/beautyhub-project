bfff7c842360cf54f59e62bb5fd1514c54ea7983
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2026-03-15 17:50:50 -0300

    feat: implementa método home no ClientController

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,7 +6,20 @@ use Illuminate\Support\Facades\Auth;
 
 class ClientController extends Controller
 {
-    public function dashboard()
+    public function home()
+    {
+        $user = Auth::user();
+
+        $nextAppointment = $user->appointments()
+            ->with(['service', 'professional.user'])
+            ->upcoming()
+            ->where('status', 'confirmed')
+            ->first();
+
+        return view('client.home', compact('nextAppointment'));
+    }
+
+    public function appointments()
     {
         $user = Auth::user();
 
@@ -21,11 +34,11 @@ class ClientController extends Controller
             ->get();
 
         $pastAppointments = $user->appointments()
-            ->with(['service', 'professional.user'])
+            ->with(['service', 'professional.user', 'review'])
             ->past()
             ->get();
 
-        return view('client.dashboard', compact(
+        return view('client.appointments', compact(
             'nextAppointment',
             'upcomingAppointments',
             'pastAppointments'
