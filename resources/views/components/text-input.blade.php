0951d9a5aae8afb862f876b44b893ca0f63e0306
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2026-03-22 22:01:43 -0300

    feat: adiciona estilos de foco ao componente de input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-1 border-violet-200 hover:border-violet-800 p-1 border-violet-400 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-1 border-violet-200 hover:border-violet-800 p-1 border-violet-400 rounded-md shadow-sm focus:border-[#6A0DAD] focus:ring-[#6A0DAD] focus:outline-none']) }}>
