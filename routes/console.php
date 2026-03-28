0ea8efe9b9856cb426b7250fe061e5d6ce13f66d
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2026-03-28 09:37:11 -0300

    feat: atualiza duração mínima para serviços

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(AutoCompleteAppointments::class)->everyFifteenMinutes();
+Schedule::job(AutoCompleteAppointments::class)->everyFiveMinutes();
