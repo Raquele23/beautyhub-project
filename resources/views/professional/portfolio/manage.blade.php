139983156be8447c1acbdaf4e8f6cfd083da7042
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2026-04-19 00:15:31 -0300

    style: centraliza a mensagem de instrução para seleção e recorte de foto

--- resources/views/professional/portfolio/manage.blade.php
+++ resources/views/professional/portfolio/manage.blade.php
@@ -71,7 +71,7 @@
                         <svg class="w-7 h-7 text-purple-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                         </svg>
-                        <span class="text-xs text-purple-400 font-medium">Clique para selecionar e recortar a foto</span>
+                        <span class="text-xs text-purple-400 font-medium text-center leading-relaxed block px-2">Clique para selecionar e<br>recortar a foto</span>
                            <input type="file" name="photo" accept="image/*" class="sr-only"
                                id="add_photo_input"
                                data-crop-target="add"
