0f60fc72d97fcb163475b19ef55dbdac8c397a9d
YK4RU <ana.lima1801@aluno.ce.gov.br>
2026-04-27 14:44:44 -0300

    style: adicionando o favicon no esqueci a senha

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="{{ asset('assets/img/Beauty_HubIcon.png') }}">
     <title>BeautyHub</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
