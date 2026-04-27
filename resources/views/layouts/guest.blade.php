d7aa82dc227b9a8bc6ecd42ce3622548ff5382f7
YK4RU <ana.lima1801@aluno.ce.gov.br>
2026-04-27 14:36:44 -0300

    style: colocando o favicon na aba

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="shortcut icon" href="{{ asset('assets/img/Beauty_HubIcon.png') }}">
         <title>{{ config('app.name', 'BeautyHub') }}</title>
 
         <!-- Fonts -->
